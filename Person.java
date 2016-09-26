9b08e3a53a36f861670d6fb312195fb0ca322e75
kimjuhee12 <hui2666@naver.com>
2016-09-26 11:47:46 +0900

    2-4 몸무게를 자신 이외에는 알 수 없도록 구현한다.

---
+++ Person.java
@@ -0,0 +1,12 @@
+class Person {
+    private float weight;
+    public void setWeight(float w) {
+        this.weight=w;
+    }
+    private float getWeight() {
+        return weight;
+    }
+}
+Person me=new Person();
+me.setWeight((float)60.0);
+System.out.println(me.getWeight());
