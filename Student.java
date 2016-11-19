d335760d507d9e5f4f7af272d77a11bdefa8c353
kimjuhee12 <hui2666@naver.com>
2016-11-19 23:53:59 +0900

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Student {
+	private String name;
+	private int stdNum;
+	
+	Student(String name, int snum) {
+		this.name = name;
+		this.stdNum = snum;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<String> snameList = new ArrayList<String>();
+		
+		Student s1 = new Student("jh 1", 1);
+		Student s2 = new Student("jh 2", 2);
+		
+		snameList.add(s2.getName());
+		snameList.add(s1.getName());
+		
+		Collections.sort(snameList);
+		
+		for (String s : snameList)
+			System.out.println(s);
+	}
+}
