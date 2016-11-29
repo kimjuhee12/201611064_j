0a9a4d08fc5d3c47e68f3aadd1cd2943bd6717ae
kimjuhee12 <hui2666@naver.com>
2016-11-29 14:22:53 +0900

    문제 oo-2: 상속을 사용하여 거북이를 구현한다

---
+++ LandTurtle.java
@@ -0,0 +1,9 @@
+package com.sd.ch8;
+//import com.sd.ch8.*;
+
+public class LandTurtle extends Turtle {
+
+	public void walk() {
+		System.out.print(" A land turtle walk...");
+		}
+}
---
+++ SeaTurtle.java
@@ -0,0 +1,9 @@
+package com.sd.ch8;
+//import com.sd.ch8.*;
+
+public class SeaTurtle extends Turtle {
+
+	public void swim() {
+		System.out.print(" A sea turtle swim...");
+		}
+}
