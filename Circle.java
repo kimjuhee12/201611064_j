e18cb149fc46306f6218c3cb4c526f8dbe4ffa03
kimjuhee12 <hui2666@naver.com>
2016-11-29 14:18:06 +0900

    문제 oo-3: 원의 면적을 계산하기

---
+++ Circle.java
@@ -0,0 +1,23 @@
+package com.sd.ch8;
+
+public class Circle extends Point {
+	private double radius;
+	
+	public Circle(int x, int y, double r) {
+		super(x,y);
+		this.radius=r;
+	}
+	
+	public double getRadius() {
+		return this.radius;
+		
+	}public double getArea() {
+		return radius * radius * Math.PI;	
+	}
+	
+	public static void main(String[] args) {
+		Circle myCircle = new Circle(10,10,5);
+		Double myArea = myCircle.getArea();
+		System.out.print("Area is " + myArea);
+	}
+}
