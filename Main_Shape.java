dd0a2015e16826b708b367ceec6b0cd5529840a5
kimjuhee12 <hui2666@naver.com>
2016-12-03 17:47:29 +0900

    Add files via upload

---
+++ Main_Shape.java
@@ -0,0 +1,8 @@
+package com.sd.ch8;
+
+public class Main_Shape {
+	public static void main(String[] args) {
+		Rectangle r = new Rectangle(3, 4);
+		System.out.println("area is " + r.calcArea() + "\nperimeter is " + r.calcPerimeter());
+	}
+}
--- Shape.java
+++ Shape.java
@@ -1,13 +1,7 @@
 package com.sd.ch8;
 
-public abstract class Shape {
-	public String name = "Shape";
-	public abstract double calcArea();
-	public abstract double calcPerimeter();
-	public String getName() {
-		return this.name;
-	}
-	public String toString() {
-		return "Shape... " + super.toString();
-	}
+public interface Shape {
+	public double calcArea();
+	
+	public double calcPerimeter();
 }
