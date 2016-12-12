00c574992dcf9819238a2da2ee49a98c4ca94c16
kimjuhee12 <hui2666@naver.com>
2016-12-12 09:16:17 +0900

    Update DiceGameGUI.java

--- DiceGameGUI.java
+++ DiceGameGUI.java
@@ -19,19 +19,18 @@ public class DiceGameGUI extends JFrame implements ActionListener {
       JLabel label1=new JLabel("Player 1");
       JLabel label2=new JLabel("Player 2");
       JLabel label3 = new JLabel("Result");
-	  // label¸í ÁöÁ¤
+	  // labelëª… ì§€ì •
       
       player1Tf=new JTextField(10);
       player2Tf=new JTextField(10);
       winnerTf = new JTextField(10);
-      // ÅØ½ºÆ® ÇÊµå ¸¸µé±â
+      // í…ìŠ¤íŠ¸ í•„ë“œ ë§Œë“¤ê¸°
       b1=new JButton("Start Game");
-      b2,b3=new JButton("Clear");
-      // ¹öÆ° ¸¸µé±â
+      b2=new JButton("Clear");
+      // ë²„íŠ¼ ë§Œë“¤ê¸°
       b1.addActionListener(this);
       b2.addActionListener(this);
-      b3.addActionListener(this);
-	  //¹öÆ° ½ÇÇà
+	  //ë²„íŠ¼ ì‹¤í–‰
       
 	  panel.add(label1);
       panel.add(label2);
@@ -50,14 +49,14 @@ public class DiceGameGUI extends JFrame implements ActionListener {
             b1.setText("Click clear button -->");
             String name1 = player1Tf.getText();
             String name2 = player2Tf.getText();
-            //player¸í ÁöÁ¤
+            //playerëª… ì§€ì •
             DiceGame dg = new DiceGame(name1, name2);
             
             dg.play();
             
             player1Tf.setText(player1Tf.getText()+" : "+String.valueOf(dg.getP1().getNum()));
             player2Tf.setText(player2Tf.getText()+" : "+String.valueOf(dg.getP2().getNum()));
-            // 'ÀÌ±ä ÇÃ·¹ÀÌ¾î¸í : °á°ú' Ãâ·Â 
+            // 'ì´ê¸´ í”Œë ˆì´ì–´ëª… : ê²°ê³¼' ì¶œë ¥ 
             winnerTf.setText(dg.score());
          }
       
